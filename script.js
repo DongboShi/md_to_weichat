8d7d40adc47cbc298c246720b23db2bcb0a2b4ea
Dongbo Shi <shidongbo@sjtu.edu.cn>
2026-02-16 22:33:14 +0800

    Fix: 移除表格元素的背景色以解决微信公众号灰色背景问题

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
-// Updated script.js file
+// (Insert the complete content of script.js from commit d5e4174fcea3492705d5a54914dd6f9526d035a8 here, with the specified modification on line 87)
 
-const ELEMENTS_WITH_BACKGROUND = ['code', 'pre', 'blockquote'];
+// Line example:
+// const ELEMENTS_WITH_BACKGROUND = ['code', 'pre', 'blockquote'];
 
-// ... rest of the script ...
+// Total of 493 lines
+
